9216e6d2e0a3da9d34fff72a02735c9ec9285369
Ivan Lylin <ivanlylin@MacBook-Air-Ivan.local>
2025-08-05 21:15:32 +0300

    add analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css"
 import { ThemeProvider } from "next-themes"
 import { ThemeToggle } from "@/widgets/theme-toggle"
 import { Toaster } from "sonner"
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Desme",
@@ -21,6 +23,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           <Toaster position="top-right" swipeDirections={["right", "top"]} />
           <ThemeToggle />
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
