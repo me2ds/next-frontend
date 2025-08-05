727ebda22a110c9ec990e6db5396bb526c01d99f
Ivan Lylin <ivanlylin@MacBook-Air-Ivan.local>
2025-08-06 00:54:11 +0300

    fix

--- src/features/user/model/logout.ts
+++ src/features/user/model/logout.ts
@@ -1,14 +1,12 @@
 "use server"
 
-import { AuthStore } from "@/entities/auth/model"
-import { UserStore } from "@/entities/user/model"
+import { httpClient } from "@/shared/api/http-client"
 import { cookies } from "next/headers"
 
 const logout = async () => {
   const cookieStore = await cookies()
   cookieStore.delete("authToken")
-  UserStore.getState().reset()
-  AuthStore.getState().setShowAuth(true)
+  await httpClient.storage.clear!()
 }
 
 export { logout }
